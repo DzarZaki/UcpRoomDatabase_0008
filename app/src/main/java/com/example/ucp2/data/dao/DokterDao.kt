5b9a66e3eaf46b74052f06d45e2b5595127092c2
DzarZaki <dzarzaki2@gmail.com>
2024-12-18 17:45:59 +0700

    menambah fungsi getAllDokter

--- app/src/main/java/com/example/ucp2/data/dao/DokterDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/DokterDao.kt
@@ -1,11 +1,16 @@
 package com.example.ucp2.data.dao
 
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.ucp2.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
 
 interface DokterDao {
 
     @Insert
     suspend fun insertDokter(dokter: Dokter)
+
+    @Query("SELECT * FROM dokter ORDER BY nama ASC")
+    fun getAllDokter(): Flow<List<Dokter>>
 
 }
