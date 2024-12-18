66ff1f6df7549c414941cfe836169682f9e037b3
DzarZaki <dzarzaki2@gmail.com>
2024-12-18 17:31:06 +0700

    Menambah PrimaryKey pada id

--- app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
+++ app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
@@ -1,10 +1,13 @@
 package com.example.ucp2.data.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 
-@Entity(tableName = "mahasiswa")
+@Entity(tableName = "jadwal")
 
 data class Jadwal (
+    @PrimaryKey
     val id :String,
     val namadokter :String,
     val namapasien :String,
