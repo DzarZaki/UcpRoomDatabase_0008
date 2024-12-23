606d97054322fa2f1813d30d2d13b396a450e21c
DzarZaki <dzarzaki2@gmail.com>
2024-12-23 22:09:45 +0700

    Menambahkan implementasi LocalRepositoryDokter untuk operasi insert dan fetch data Dokter menggunakan DokterDao

---
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryDokter.kt
@@ -0,0 +1,18 @@
+package com.example.ucp2.repository
+
+
+import com.example.ucp2.data.dao.DokterDao
+import com.example.ucp2.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryDokter(
+    private val dokterDao: DokterDao
+) : RepositoryDokter {
+    override suspend fun insertDokter(dokter: Dokter) {
+        dokterDao.insertDokter(dokter)
+    }
+
+    override fun getAllDokter(): Flow<List<Dokter>> {
+        return dokterDao.getAllDokter()
+    }
+}
