134d60c0835aad56faf3a19ab6c8a720ab89141d
DzarZaki <dzarzaki2@gmail.com>
2024-12-24 09:16:28 +0700

    Menambahkan @Dao

--- app/src/main/java/com/example/ucp2/data/dao/DokterDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/DokterDao.kt
@@ -1,10 +1,12 @@
 package com.example.ucp2.data.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.ucp2.data.entity.Dokter
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface DokterDao {
 
     @Insert
